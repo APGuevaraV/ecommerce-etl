504eab2e4c1aa92a28160aa5b0a5a565f76681f1
Ana Paula <ana.guevara.valladares@gmail.com>
2025-08-08 16:50:27 -0500

    grafico dona 7 primeros paises con mayor cantidad de pedidos

--- graphics/validacion.py
+++ graphics/validacion.py
@@ -24,5 +24,17 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
+ventas_por_paises = df.groupby('Country')[
+    'Quantity'].sum().sort_values(ascending=False)
+top_n = 7
+ventas_paises_reducido = ventas_por_paises[:top_n]
+ventas_paises_reducido['Otros'] = ventas_por_paises[top_n:].sum()
 
+ventas_paises_reducido.plot(kind='pie',
+                            autopct='%1.1f%%',
+                            figsize=(7, 7),
+                            wedgeprops={'width': 0.4})
+plt.title("Porcentaje de pedidos por país")
+plt.ylabel("")
+plt.show()
 conn.close()
